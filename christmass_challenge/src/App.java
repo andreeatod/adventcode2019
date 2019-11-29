064aa819508c53eb7e27d110528ec941d4eaa24c
Andreea T <andreeatodasca@gmail.com>
2019-11-29 20:48:13 +0200

    Mode details about the project

--- christmass_challenge/src/App.java
+++ christmass_challenge/src/App.java
@@ -2,5 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello there!");
+        System.out.println("This will be my try for AdventOfCode2019!");
+        System.out.println("Visit adventofcode.com for more details.");
     }
 }
