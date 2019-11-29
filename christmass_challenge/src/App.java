eb5266054a1a32fea0daa36d1eaa98a07a222644
Andreea T <andreeatodasca@gmail.com>
2019-11-29 21:17:28 +0200

    Changed link

--- christmass_challenge/src/App.java
+++ christmass_challenge/src/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello there!");
         System.out.println("This will be my try for AdventOfCode2019!");
-        System.out.println("Visit adventofcode.com for more details.");
+        System.out.println("Visit https://adventofcode.com for more details.");
     }
 }
