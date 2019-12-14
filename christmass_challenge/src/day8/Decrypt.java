166c4f3584b243d6b93c8f882df2401477c2eddd
Andreea T <andreeatodasca@gmail.com>
2019-12-14 16:47:03 +0200

    Part II of day 8 challenge

--- christmass_challenge/src/day8/Decrypt.java
+++ christmass_challenge/src/day8/Decrypt.java
@@ -2,18 +2,19 @@ package day8;
 
 import common.ReadFromFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Decrypt {
+
+    private static int width = 25;
+    private static int height = 6;
+    private static int perLayer = width * height;
 
     public static void main(String[] args) {
         String fileName = "christmass_challenge/src/day8/dayEight.txt";
 
         List<Integer> input = ReadFromFile.readCharacterAsIntegers(fileName);
-
-        int width = 25;
-        int height = 6;
-        int perLayer = width * height;
 
         int layer = 0, product = 1;
         int min = Integer.MAX_VALUE;
@@ -21,7 +22,7 @@ public class Decrypt {
         for (int i = 0; i < input.size(); ) {
             int zeros = 0, ones = 0, twos = 0;
 
-            while (i < perLayer * (layer + 1) && i <input.size()) {
+            while (i < perLayer * (layer + 1) && i < input.size()) {
                 if (input.get(i) == 0) {
                     zeros++;
                 }
@@ -43,5 +44,35 @@ public class Decrypt {
         }
 
         System.out.println("The number " + product);
+
+        List<Integer> image = new ArrayList<>(perLayer);
+        for (int i = 0; i < perLayer; i++) {
+            image.add(input.get(i));
+        }
+
+        completeImage(input, image);
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (!image.get(i * width + j).equals(0)) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void completeImage(List<Integer> input, List<Integer> image) {
+        for (int i = 0; i < image.size(); i++) {
+            if (image.get(i) == 2) {
+                int k = 0;
+                while (input.get(perLayer * k + i) == 2) {
+                    k++;
+                }
+                image.set(i, input.get(perLayer * k + i));
+            }
+        }
     }
 }
